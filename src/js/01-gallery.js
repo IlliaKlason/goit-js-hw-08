fdd12fa239685c3b7b0183bca7a371435600f363
IlliaKlason <105161428+IlliaKlason@users.noreply.github.com>
2022-08-09 23:04:25 +0300

    add task 1-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+const gallery = document.querySelector(".gallery")
+
+gallery.insertAdjacentHTML(
+   "afterbegin",
+   galleryItems
+      .map(
+         ({ original, preview, description }) =>
+            `<a class="gallery__item" href="${original}">
+        <img class="gallery__image"
+        src="${preview}" 
+        alt= "${description}"></a>`
+      )
+      .join("")
+);
+
+
+
+let lightbox = new SimpleLightbox(".gallery a", {
+   captionsData: "alt",
+   captionDelay: 250,
+   captionPosition: 'bottom',
+   close: false,
+   showCounter: false,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const video = document.querySelector("#vimeo-player");
+
+
+const player = new Player(video);
+
+player.on(
+   "timeupdate",
+   throttle(function playTime(e) {
+      localStorage.setItem("video-time", e.seconds)
+   }, 1000),
+);
+
+player.setCurrentTime(localStorage.getItem('video-time'));
